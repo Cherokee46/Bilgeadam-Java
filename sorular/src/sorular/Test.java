5e3fa0d1b1ca2fd62ea90344417fafbf176f2dac
Cherokee46 <136843504+Cherokee46@users.noreply.github.com>
2023-10-22 22:37:54 +0300

    Add files via upload

---
+++ sorular/src/sorular/Test.java
@@ -0,0 +1,83 @@
+package sorular;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// Soru 11
+		
+		String str1 = new String("Core");
+		String str2 = new String("CoRe");
+		
+		System.out.println(str1 = str2);
+		//Soru 10
+		//short [] args = new short[] {50,50};
+		
+		// Soru 9
+		
+//		String[] arr = {"I","N","S","E","R","T"};
+//		
+//		for (int n = 1; n < arr.length; n+=2) {
+//			if(n % 2 == 0) {
+//				continue;
+//			}
+//			
+//			System.out.print(arr[n]);
+//		}
+		
+//		int n = 1;
+//		n++;
+//		n += 1;
+//		
+//		String swiftCode = "ICICINBBRT4";
+//		System.out.println(swiftCode.substring(4,6));
+		
+		//Soru 6
+		//double          arr     [] = new int[2];
+		
+		// Soru 5
+		
+//		double price = 90000;
+//		String model;
+//		
+//		if(price > 10000) {
+//			model = "Tesla X";
+//		}else if(price <= 100000) {
+//			model = "Tesla Y";
+//		}else {
+//			model=null;
+//		}
+//		System.out.println(model);
+		//Soru 4
+		
+//		Double[] d = new Double[2];
+//		
+//		Double d1 = 1.0D;
+//		
+//		d[0] = 1.0;
+//		d[1] = 2.0;
+//		
+//		System.out.println(d[2] + d[1]);
+//		
+		//Soru 3
+		
+//		int a = 100;
+//		System.out.println(-a++); // -1.a > a++ //  a *= -1
+//		System.out.println(-++a);
+		
+		// Soru 2
+		//System.out.println("metin " + (10 != 5));
+		
+		// Soru1
+//		String s1 = "OCAJP";
+//		String s2 = "OCAJP" + "";
+//		
+//		System.out.println(s1 == s2);
+//		
+//		Integer int1 = 5;
+//		Integer int2 = 4 + 1;
+//		
+//		System.out.println(int1 == int2); // Integer veri tipi için "==" operatörü değerleri mi yoksa
+		// nesne referanslarını mı karşılaştırır.
+	}
+
+}
